cac67bdffaa00bc3d7ea4c3150f61e983b754b90
Zeraku-silent <shaman-96@inbox.ru>
2023-10-07 00:35:12 +0700

    Add new components

--- src/components/List/Title/List/List.jsx
+++ src/components/List/Title/List/List.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import { Input } from "../../../Input/Input";
+import { TasksList } from "./List.styles";
+import { Task } from "./Task";
 
 export const List = () => {
+  const [tasks, setTasks] = useState([]);
+
   return (
     <div>
       <Input />
+      <TasksList>
+        {tasks.map((item) => (
+          <Task task={item} key={item.id} />
+        ))}
+      </TasksList>
     </div>
   );
 };
---
+++ src/components/List/Title/List/List.styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const TasksList = styled.div`
+  background-color: lightblue;
+`;
