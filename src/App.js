acd702a95152734f69defcb3533b35fd17a706f1
Zeraku-silent <shaman-96@inbox.ru>
2023-10-31 00:11:38 +0700

    Learn React

--- src/App.js
+++ src/App.js
@@ -6,9 +6,23 @@ const Card = ({ children }) => {
   return <div className="card">{children}</div>;
 };
 
+const Item = ({ name, isPackced }) => {
+  return (
+    <li className="item">{isPackced ? <del>{name + "✔"} </del> : name} </li>
+  );
+};
+
 function App() {
   return (
     <div className="App">
+      <section>
+        <h1> Sally Ride's Packing List</h1>
+        <ul>
+          <Item isPackced={true} name="Space suit" />
+          <Item isPackced={true} name="Helmet with a golden leaf" />
+          <Item isPackced={false} name="Photo of Tam" />
+        </ul>
+      </section>
       <Title />
       <List />
     </div>
