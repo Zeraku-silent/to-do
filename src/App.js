e6f0419a4b8c8b02fed51d357ca04570a2f2d623
Zeraku-silent <shaman-96@inbox.ru>
2023-11-01 23:11:32 +0700

    Learn React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Title } from "./components/Title";
 import { List } from "./components/List";
 import { Avatar } from "./components/LearnReact/Avatar";
+import { nanoid } from "nanoid";
+import { getImageUrl } from "./components/LearnReact/utils";
 
 const Card = ({ children }) => {
   return <div className="card">{children}</div>;
@@ -15,6 +17,57 @@ const Item = ({ name, isPackced }) => {
 };
 
 function App() {
+  const people = [
+    {
+      id: 0,
+      name: "Creola Katherine Johnson",
+      profession: "mathematician",
+      accomplishment: "spaceflight calculations",
+      imageId: "MK3eW3A",
+    },
+    {
+      id: 1,
+      name: "Mario José Molina-Pasquel Henríquez",
+      profession: "chemist",
+      accomplishment: "discovery of Arctic ozone hole",
+      imageId: "mynHUSa",
+    },
+    {
+      id: 2,
+      name: "Mohammad Abdus Salam",
+      profession: "physicist",
+      accomplishment: "electromagnetism theory",
+      imageId: "bE7W1ji",
+    },
+    {
+      id: 3,
+      name: "Percy Lavon Julian",
+      profession: "chemist",
+      accomplishment:
+        "pioneering cortisone drugs, steroids and birth control pills",
+      imageId: "IOjWm71",
+    },
+    {
+      id: 4,
+      name: "Subrahmanyan Chandrasekhar",
+      profession: "astrophysicist",
+      accomplishment: "white dwarf star mass calculations",
+      imageId: "lrWQx8l",
+    },
+  ];
+  const chemists = people.filter((person) => person.profession === "chemist");
+
+  const listItems = chemists.map((person) => (
+    <li>
+      <img src={getImageUrl(person)} alt={person.name} />
+      <p>
+        <b>{person.name}:</b>
+        {" " + person.profession + " "}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  ));
+
   return (
     <div className="App">
       <section>
@@ -25,6 +78,7 @@ function App() {
           <Item isPackced={false} name="Photo of Tam" />
         </ul>
       </section>
+      <ul>{listItems}</ul>
       <Title />
       <List />
     </div>
