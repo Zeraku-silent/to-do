c670ed1634fb12c05ced4571d5902d42a7466438
Zeraku-silent <shaman-96@inbox.ru>
2023-11-03 00:10:36 +0700

    Learn React

--- src/App.js
+++ src/App.js
@@ -74,8 +74,27 @@ function App() {
     </li>
   ));
 
+  const Recipe = ({ drinkers }) => {
+    return (
+      <ol>
+        <li>Boil {drinkers} cups of water.</li>
+        <li>
+          Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.
+        </li>
+        <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
+      </ol>
+    );
+  };
+
   return (
     <div className="App">
+      <section>
+        <h1>Spiced Chai Recipe</h1>
+        <h2>For two</h2>
+        <Recipe drinkers={2} />
+        <h2>For gathering</h2>
+        <Recipe drinkers={4} />
+      </section>
       <section>
         <h1> Sally Ride's Packing List</h1>
         <ul>
